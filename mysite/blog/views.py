dbd93a6f5ad9ff0edc1fee538d2869178eb2510b
honeyVoid <116582081+honeyVoid@users.noreply.github.com>
2023-09-13 13:50:00 +0300

    updated models,urls,views, added pagination

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,24 +1,43 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import Http404
-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
 from blog.models import Post
 
 
-def post_list(request):
-    posts = Post.published.all()
-    return render(
-        request,
-        'blog/post/list.html',
-        {'posts': posts}
-    )
+# def post_list(request):
+#     posts_list = Post.published.all()
+#     paginator = Paginator(posts_list, 3)
+#     page_number = request.GET.get('page', 1)
+
+#     try:
+#         posts = paginator.page(page_number)
+#     except EmptyPage:
+#         posts = paginator.page(paginator.num_pages)
+#     except PageNotAnInteger:
+#         posts = paginator.page(1)
+
+#     return render(
+#         request,
+#         'blog/post/list.html',
+#         {'posts': posts}
+#     )
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
 
 
-def post_detail(request, id):
+def post_detail(request, year, month, day, post):
     post = get_object_or_404(
         Post,
-        id=id,
-        status=Post.Status.PUBLISHED
+        status=Post.Status.PUBLISHED,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day
     )
 
     return render(
