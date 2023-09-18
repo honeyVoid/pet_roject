4850003b72740de8471c9fbda80ae879e8642c7d
honeyVoid <116582081+honeyVoid@users.noreply.github.com>
2023-09-18 21:52:55 +0300

    added  some template tags, updated teplate

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,6 +3,7 @@ from django.views.generic import ListView
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db.models import Count
 
 from taggit.models import Tag
 
@@ -18,7 +19,7 @@ def post_list(request, tag_slug=None):
         tag = get_object_or_404(Tag, slug=tag_slug)
         posts_list = posts_list.filter(tags__in=[tag])
 
-    paginator = Paginator(posts_list, 3)
+    paginator = Paginator(posts_list, 10)
     page_number = request.GET.get('page', 1)
 
     try:
@@ -56,13 +57,25 @@ def post_detail(request, year, month, day, post):
     )
     comments = post.comments.filter(active=True)
     form = ComentForm()
+
+    post_tags_ids = post.tags.values_list('id', flat=True)
+
+    similar_posts = Post.published.filter(
+        tags__in=post_tags_ids
+    ).exclude(id=post.id)
+
+    similar_posts = similar_posts.annotate(
+        same_tags=Count('tags')
+    ).order_by('-same_tags', '-publish')[:4]
+
     return render(
         request,
         'blog/post/detail.html',
         {
             'post': post,
             'comments': comments,
-            'form': form
+            'form': form,
+            'similar_posts': similar_posts
         }
     )
 
